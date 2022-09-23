6d9b17703f5d311e26ff8ba04225a262866f723b
meryem <mmeryemisik@gmail.com>
2022-09-23 23:58:51 +0300

    started login page

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template >
+    <div class="login-page py-5">
+       <div class="container login-page-content">
+            <div class="row ">
+                <div class="col-4">
+                    beyas
+                </div>
+                <div class="col-8 login-page-form">
+                   <h5 class="login-page-header">Create Account</h5>
+                </div>
+            </div>
+       </div>
+    </div>
+</template>
+
+<style  lang="scss">
+@import '../assets/css/login.scss';
+
+</style>
