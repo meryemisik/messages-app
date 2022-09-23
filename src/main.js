0783fb5fec529196eb5f0c2403f29c8b8546eee9
meryem <mmeryemisik@gmail.com>
2022-09-23 20:23:06 +0300

    created message-app project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import BootstrapVue3 from "bootstrap-vue-3";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+createApp(App).use(router).use(BootstrapVue3).mount("#app");
