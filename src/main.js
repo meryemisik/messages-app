92e92a272cd62d961f0a38f9d76d19690f93e40a
meryem <mmeryemisik@gmail.com>
2022-09-25 20:48:52 +0300

    added simple-line icons

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from "./router";
 import BootstrapVue3 from "bootstrap-vue-3";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
-
+import "simple-line-icons"
 const app = createApp(App);
 createApp(App).use(router).use(BootstrapVue3).mount("#app");
